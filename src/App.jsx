3a3699a24fdd1ef03899de6d5a8b24bfe672abe9
Mujjamil <jamadarmj5@gmail.com>
2026-03-14 19:32:06 +0530

    changed username for pushing the code

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
     return (
         <div className="flex-center h-[100vh]">
-            <h1 className="text-3xl text-indigo-300">HEllo , Gsap</h1>
+            <h1 className="text-3xl text-indigo-300">HEo , Gsap</h1>
         </div>
     )
 }
